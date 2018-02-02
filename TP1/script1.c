4c92068eb96ae93bea1f719ab8ad25a3e938cebc
Schrut <gimflorian@gmail.com>
2018-02-02 11:37:13 +0100

    Delits-cas done

--- TP1/script1.c
+++ TP1/script1.c
@@ -31,7 +31,7 @@ int display_mode;
 int xpos;
 int ypos;
 int mouse_is_inside;
-int* transistion;
+int* transition;
 const char* int_ext;
 
 
@@ -71,21 +71,66 @@ void glPrintText(int x, int  y, const char * text)
 		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, text[i]);
 }
 
-/*Fonction permettant de traiter les cas non évidents*/
-int delit_cas (int i, int x, int y)
+Poly madeAVector (Poly A, Poly B)
 {
-  double a, b, _x;
+  Poly new_poly;
+  new_poly.coord[0] = B.coord[0] - A.coord[0];
+  new_poly.coord[1] = B.coord[1] - A.coord[1];
+  return new_poly;
+}
 
-  if (mon_poly[i+1].coord[1] == mon_poly[i].coord[1]) return 1;
+int isASecant (void)
+{
+  int i;
+  int signe1 , signe2;
+  Poly ac, bc, ad, bd;
+  mon_poly[nb_point].coord[0] = mon_poly[0].coord[0];
+  mon_poly[nb_point].coord[1] = mon_poly[0].coord[1];
 
-  a = (double)( mon_poly[i+1].coord[1] - mon_poly[i].coord[1] ) / (double)( mon_poly[i+1].coord[0] - mon_poly[i].coord[0] );
-  b = (double) mon_poly[i+1].coord[1] - (mon_poly[i+1].coord[0]*a);
+  for (i=0 ; i < nb_point+1 -3 ; i++)
+  {
+    ac = madeAVector(mon_poly[i], mon_poly[i + 2]);
+    ad = madeAVector(mon_poly[i], mon_poly[i + 3]);
+    bc = madeAVector(mon_poly[i + 1], mon_poly[i + 2]);
+    bd = madeAVector(mon_poly[i + 1], mon_poly[i + 3]);
 
-  _x = ( y - b ) / a;
+    if (ac.coord[0] * ad.coord[1] - ad.coord[0] * ac.coord[1] > 0 ) signe1 = 1;
+    else signe1 = 0;
+    
+    if (bc.coord[0] * bd.coord[1] - bd.coord[0] * bc.coord[1] > 0 ) signe2 = 1;
+    else signe2 = 0;
 
-  if (_x > x) return 1;
-  else return 0;
+    if (signe1 != signe2){printf("Secant /!\\ entre %d-%d et %d-%d\n",i,i+1,i+2,i+3); return i;}
+    else return 0;
 
+
+  }
+}
+
+/*Fonction permettant de traiter les cas non évidents*/
+int delit_cas (int i, int x, int y, int choix)
+{
+  if (choix)
+  {
+    double a, b, _x;
+
+    if (mon_poly[i+1].coord[1] == mon_poly[i].coord[1]) return 1;
+
+    a = (double)( mon_poly[i+1].coord[1] - mon_poly[i].coord[1] ) / (double)( mon_poly[i+1].coord[0] - mon_poly[i].coord[0] );
+    b = (double) mon_poly[i+1].coord[1] - (mon_poly[i+1].coord[0]*a);
+
+    _x = ( y - b ) / a;
+
+    if (_x > x) return 1;
+    else return 0;
+  }
+
+  else {
+    if (transition[i + 1] == transition[i])
+      return 0;
+    else if ( (transition[i - 1] == 1 && transition[i + 1] == 2) || (transition[i - 1] == 2 && transition[i + 1] == 1) ) return 1;
+    else return 0; 
+  }
 }
 
 /*Fonction qui nous renseigne si un point est dans le Polygone*/
@@ -93,23 +138,30 @@ int isInside(int x, int y)
 {
   int i;
   int NI=0;
+  
   mon_poly[nb_point].coord[0] = mon_poly[0].coord[0];
   mon_poly[nb_point].coord[1] = mon_poly[0].coord[1];
+
   for (i = 0 ; i < nb_point+1 ; i++){
-    if (mon_poly[i].coord[0] > x && mon_poly[i].coord[1] > y) transistion[i] = 1;
-    else if (mon_poly[i].coord[0] > x && mon_poly[i].coord[1] < y) transistion[i] = 2;
-    else if (mon_poly[i].coord[0] < x && mon_poly[i].coord[1] < y) transistion[i] = 3;
-    else if (mon_poly[i].coord[0] < x && mon_poly[i].coord[1] > y) transistion[i] = 4;
-    else {transistion[i] = 5;}
+    if (mon_poly[i].coord[0] >= x && mon_poly[i].coord[1] > y)       transition[i] = 1;
+    else if (mon_poly[i].coord[0] > x && mon_poly[i].coord[1] < y)  transition[i] = 2;
+    else if (mon_poly[i].coord[0] < x && mon_poly[i].coord[1] < y)  transition[i] = 3;
+    else if (mon_poly[i].coord[0] < x && mon_poly[i].coord[1] > y)  transition[i] = 4;
+    else if (mon_poly[i].coord[0] > x && mon_poly[i].coord[1] == y) transition[i] = 5;
+    else transition[i] = 6;
   }
+
   for (i = 0 ; i < nb_point ; i++){
-    if ( (transistion[i] == 1 && transistion[i+1] == 2) || (transistion[i] == 2 && transistion[i+1] == 1) )
+    if ( (transition[i] == 1 && transition[i+1] == 2) || (transition[i] == 2 && transition[i+1] == 1) )
       NI++;
-    else if ( (transistion[i] == 2 && transistion[i+1] == 4) || (transistion[i] == 4 && transistion[i+1] == 2) )
-      NI += delit_cas(i, x, y);
-    else if ( (transistion[i] == 1 && transistion[i+1] == 3) || (transistion[i] == 3 && transistion[i+1] == 1) )
-      NI += delit_cas(i, x, y);
+    else if ( (transition[i] == 2 && transition[i+1] == 4) || (transition[i] == 4 && transition[i+1] == 2) )
+      NI += delit_cas(i, x, y, 1);
+    else if ( (transition[i] == 1 && transition[i+1] == 3) || (transition[i] == 3 && transition[i+1] == 1) )
+      NI += delit_cas(i, x, y, 1);
+    else if ( transition[i] == 5 )
+      NI += delit_cas(i, x, y, 0);
   }
+
   if ( NI%2 != 0 && NI != 0) return 1;
   else return 0;
 }
@@ -199,9 +251,9 @@ void displayGL()
   int i;
   glClear(GL_COLOR_BUFFER_BIT);
 
+  printf("secant : %d\n",isASecant());
 
   glPrintText(10, winY - 10, int_ext);
-
 
   for (i = 0; i < nb_point; i++)
   {
@@ -340,7 +392,7 @@ void init()
   display_mode = 3;
   mouse_is_inside = 0;
   int_ext = "Exterieur";
-  transistion = malloc((nb_point_max + 1) * sizeof(Poly));
+  transition = malloc((nb_point_max + 1) * sizeof(Poly));
   mon_poly = malloc (nb_point_max * sizeof(Poly));
 
   for (i=0 ; i < nb_point_max ; i++)
@@ -387,7 +439,7 @@ int main (int _argc, char ** _argv){
   posX = (glutGet(GLUT_SCREEN_WIDTH ) - winX) / 2;
 	posY = (glutGet(GLUT_SCREEN_HEIGHT) - winY) / 2;
 
-  //glfwGetMousePos(&m_xpos, &m_ypos);
+  
   glutInitWindowSize(winX, winY);
 	glutInitWindowPosition(posX, posY);
 
